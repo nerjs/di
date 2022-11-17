b3f42f66c9f7bfb59e1b1dceb7b968d10c6e7633
Alexey <nerjs.stap@gmail.com>
2022-11-17 12:33:34 +0100

    test app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '../di/decorators/module.decorator'
+import { AuthModule } from './auth/auth.module'
+import { UserModule } from './user/user.module'
 
-@Module()
+@Module({
+  imports: [
+    UserModule,
+    {
+      predicate: true,
+      module: AuthModule,
+    },
+  ],
+})
 export class AppModule {}
